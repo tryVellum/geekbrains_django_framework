ec2935b65ce7ca6e406cc70a13050ff51cc5382a
tryVellum <amigodrigo@gmail.com>
2021-02-24 15:56:06 +0300

    add new file
    Start project, work geekshop-server. Выполняя 2 урок натворил фигни.

---
+++ geekshop-server/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/And/Desktop/geekbrains_django_framework/geekshop-server/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
